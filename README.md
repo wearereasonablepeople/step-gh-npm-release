1e1283c436c53827a1396f22ff5b1e692b72528d
Onur Gunduz <ogunduz@gmail.com>
2018-02-23 12:12:54 +0100

    Add scripts

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# step-gh-npm-release
-a wercker step for releasing a new version
+# wercker-step-gh-npm-release
+
+## options
+
+* `ghtoken` needs to be set to your github API token
+* `npmtoken` needs to be set to your npm auth token
+* `branch` specify the branch to publish
+
+## example
+
+```
+publish:
+  box:
+    id: acroyear2/karma-box:8.5.0
+  steps:
+    - install-packages:
+        packages: git ssh-client
+    - wearereasonablepeople/gh-npm-release@0.1.10:
+        ghtoken: $GITHUB_TOKEN
+        npmtoken: $NPM_TOKEN
+        branch: release
+```
---
+++ run.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+if [ ! -n "$WERCKER_GH_NPM_RELEASE_GHTOKEN" ]; then
+  fail "missing option \"ghtoken\""
+fi
+
+if [ ! -n "$WERCKER_GH_NPM_RELEASE_NPMTOKEN" ]; then
+  fail "missing option \"npmtoken\""
+fi
+
+if [ 'github.com' = "$WERCKER_GIT_DOMAIN" ]; then
+  repo="$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY"
+else
+  fail "missing option \"repo\""
+fi
+
+info "using \"$repo\""
+
+remote="https://$WERCKER_GH_NPM_RELEASE_GHTOKEN@github.com/$repo.git"
+
+info "using \"$remote\""
+
+if [ -n "$WERCKER_GH_NPM_RELEASE_BRANCH" ]; then
+  branch="$WERCKER_GH_NPM_RELEASE_BRANCH"
+else
+  branch="release"
+  echo "$repo" | grep -qE "$WERCKER_GIT_OWNER\/$WERCKER_GIT_OWNER\.github\.(io|com)"
+  if [ $? -eq 0 ]; then
+      branch="master"
+  fi
+fi
+
+info "using branch \"$branch\""
+
+git config user.email "pleaseemailus@wercker.com"
+git config user.name "werckerbot"
+
+git config push.default simple
+
+git remote set-url origin "$remote"
+
+git checkout "$branch"
+
+git merge "$WERCKER_GIT_BRANCH"
+
+VERSION=$(node -p "require('./package.json').version" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+
+yarn version --new-version "$VERSION"
+
+git push && git push --tags
+
+if [ $? -ne 0 ]; then
+  fail "failed deploying a new version to github"
+fi
+
+touch .npmrc
+
+echo "//registry.npmjs.org/:_authToken=$WERCKER_GH_NPM_RELEASE_NPMTOKEN" >> .npmrc
+
+while read pkg; do
+  name=$(node -p "require('./package.json').name")
+  current=$(node -e "console.log(require(\"$pkg\").version)")
+  published=$(npm view "$name" version 2>/dev/null)
+  if [ "$published" != "$current" ]; then
+    node -e "p=require(\"$pkg\"); (p.scripts && p.scripts.build) ? process.exit(0) : process.exit(1)"
+    if [ $? -eq 0 ]; then
+      yarn && yarn build
+    fi
+    yarn publish --access "$WERCKER_GH_NPM_RELEASE_ACCESS" --new-version "$VERSION"
+  fi
+done < <(find . -name package.json)
+
+if [ $? -ne 0 ]; then
+  fail "failed deploying a new version to npm"
+else
+  success "ok"
+fi
